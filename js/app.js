fbb485ea0f2fc56c04b44d0ee1a87c6bfd85077d
Victor Luo <victorluo@MacBook-Air.local>
2020-03-26 10:25:19 -0700

    fix restart button, basic concept fully functional

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ $("#start").on('click', startGame);
 function startGame() {
     $(".title").css({'display': 'none'});
     $(".start").css({'display': 'block'});
+    // $(".start").first().css({'display': 'block'});
 }
 
 $("#restart").on('click', restartGame);
@@ -36,7 +37,7 @@ function handleClick(event) {
     if ($(event.target).hasClass("ch1")) {currentScore = currentScore - 1};
     if ($(event.target).hasClass("ch3")) {currentScore = currentScore + 1};
     $(this).parent().css({'display': 'none'});
-    $(this).parent().parent().next().css({'display': 'block'});
+    $(this).parent().next().css({'display': 'block'});
     if (currentScore >= 1) {$(".goodend").css({'display': 'block'}); $(".badend").css({'display': 'none'})} 
         else {$(".goodend").css({'display': 'none'}); $(".badend").css({'display': 'block'})}
 }
