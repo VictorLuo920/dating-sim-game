ddc53cec539f6db5bcfce8371bd6eb370e266fe3
Victor Luo <victorluo@MacBook-Air.local>
2020-03-26 10:28:10 -0700

    error in commit

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ function handleClick(event) {
     if ($(event.target).hasClass("ch1")) {currentScore = currentScore - 1};
     if ($(event.target).hasClass("ch3")) {currentScore = currentScore + 1};
     $(this).parent().css({'display': 'none'});
-    $(this).parent().parent().next().css({'display': 'block'});
+    $(this).parent().next().css({'display': 'block'});
     if (currentScore >= 1) {$(".goodend").css({'display': 'block'}); $(".badend").css({'display': 'none'})} 
         else {$(".goodend").css({'display': 'none'}); $(".badend").css({'display': 'block'})}
 }
